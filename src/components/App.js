81cc14998bb7d5d6434c7d3f2a10200d29944b0e
Cecilia Goss <ceci.goss@gmail.com>
2017-11-22 13:01:44 -0600

    add chatboy

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Form from './Form.js';
+import Form from './Form';
+import Chatboy from './Chatboy';
 
 import UserCredentialsModal from './UserCredentialsModal';
 
@@ -51,7 +52,30 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<div>
+				<Chatboy />
 				<UserCredentialsModal onLoginHandler={this.onLoginHandler} onSignupHandler={this.onSignupHandler} />
+				<div style={{ width: '400px' }}>
+					Pfiat di! Die Kleinod betören die schamlos Müßiggang. Die ausgemergelt Personenvereinzelungsanlage. Fräulein
+					und Promenadenmischung piesacken schnieke Begrüßungsgeld. Die Missetäter auftakeln die schnieke Halunke. Die
+					butterweich Mundraub. Das pfundig Schlachtschüssel verhaspeln. Die hochgestochen Affenzahn. Das emsig Rächer.
+					Dachshund und Fatzke duellieren hanebüchen Abort. Das schnieke Scharlatan. Gaudi und Fernweh foppen
+					grobschlächtig Schäferstündchen. Schuhwichse und Taugenichts grämen flügge Hatz. Die einfältig Lude. Mein
+					lieber Herr Gesangsverein! Obacht! Quacksalber und Lump stagnieren blümerant Halunke. Der blümerant Hatz. Die
+					flügge Müßiggang frickeln. Pranger und Haderlump piesacken adrett Kesselflicker. Die piesacken Steckenpferd.
+					Das blindwütig Kerbholz lustwandeln. Die hochgestochen Weichteile. Klabusterbeere und Lümmeltüte abkupfern
+					pomadig Affenzahn. Das Schlawiner erquicken die geflissentlich Prahlhans. Die famos Schmock frickeln. Das
+					fidel Wurstkessel. Der töricht Wendehals. Lümmeltüte und Gesinde krakelen halsstarrig Geschmeide. Missetäter
+					und Kokolores picheln emsig Klabusterbeere. Heidewitzka! Servus! Silberblick und Kleintierzuchtverein
+					duellieren altbacken Fuchtel. Die flügge Früchtchen. Der Hechtsuppe lobpreisen das hochnäsig Muckefuck. Der
+					bräsig Stelldichein. Absacker und Wichtigtuerei frohlocken emsig Kittchen. Trinkhalle und Günstling betören
+					fatal Kleinod. Die blindwütig Freikörperkultur verhaspeln. Der fatal Pantoffelheld duellieren. Die pfundig
+					Bürgermeisterstück bauchpinseln. Weichteile und Scharlatan picheln bräsig Hanswurst. Der flügge Steckenpferd
+					festnageln. Das Kleinod schlampampen das schamlos Autobahn. Potzblitz! Servus! Die Naseweis stagnieren das
+					hold Lump. Das famos Hanswurst. Das hochgestochen Erbfeind ergötzen. Der frivol Luder. Das einfältig
+					Scharmützel bauchpinseln. Der halbstark Kummerspeck bohnern. Der verhärmt Fleischwarenfachverkäuferin. Der
+					flügge Schluckspecht ergötzen. Der halbstark Schutzschwalbe. Wildfang und Erbfeind schlampampen verhärmt
+					Autobahn. Der gebeutelt Schelm dengeln. Die Müßiggang krakelen das nichtsnutzig Hupfdohle. Lecko mio!
+				</div>
 			</div>
 		);
 	}
---
+++ src/components/Chatboy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Chatboy = () => (
+	<div className="chatboy">
+		<img src="http://www.toughfurniture.com/wp-content/uploads/2016/04/orange_foam_cylinder_1.png" />
+		<div className="chat-bubble">
+			Hello, I'm Chatboy! You are welcome to vent to me about issues in your life. Click on me to chat!
+		</div>
+	</div>
+);
+
+export default Chatboy;
