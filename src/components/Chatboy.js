98d3e334749c0f1437951b2ff06c6f653344d5d5
Cecilia Goss <ceci.goss@gmail.com>
2017-11-22 14:26:57 -0600

    add to chatboy and change styles so that messages height stretches upwards

--- src/components/Chatboy.js
+++ src/components/Chatboy.js
@@ -1,12 +1,76 @@
 import React from 'react';
+import { Divider, Form, Button } from 'semantic-ui-react';
 
-const Chatboy = () => (
-	<div className="chatboy">
-		<img src="http://www.toughfurniture.com/wp-content/uploads/2016/04/orange_foam_cylinder_1.png" />
-		<div className="chat-bubble">
-			Hello, I'm Chatboy! You are welcome to vent to me about issues in your life. Click on me to chat!
-		</div>
-	</div>
-);
+export default class Chatboy extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			chatMode: false,
+			messages: [],
+			currentMessage: '',
+		};
+		this.welcomeMessage =
+			"Hello, I'm Chatboy! You are welcome to vent to me about issues in your life. Click on me to chat!";
+	}
 
-export default Chatboy;
+	chat = () => {
+		return (
+			<div className="chat">
+				{this.messages()}
+				<Form.Input
+					className="chat-input"
+					size="mini"
+					value={this.state.currentMessage}
+					type="text"
+					onChange={e => {
+						this.setState({ currentMessage: e.target.value });
+					}}
+				/>
+				<Button
+					className="chat-send"
+					onClick={e => {
+						let messages = this.state.messages.slice();
+						messages.push(this.state.currentMessage);
+						this.setState({
+							messages: messages,
+							currentMessage: '',
+						});
+					}}
+				>
+					Send
+				</Button>
+			</div>
+		);
+	};
+
+	messages = () => {
+		let messages = this.state.messages.slice();
+		return (
+			<div className="messages">
+				{messages.map((message, index) => (
+					<div key={'message-' + index}>
+						{message}
+						<Divider fitted={true} />
+					</div>
+				))}
+			</div>
+		);
+	};
+
+	render() {
+		return (
+			<div className="chatboy">
+				<div className="chat-bubble">
+					<div className="text">{this.state.chatMode ? this.chat() : this.welcomeMessage}</div>
+					<div className="tip" />
+				</div>
+				<img
+					src="http://www.toughfurniture.com/wp-content/uploads/2016/04/orange_foam_cylinder_1.png"
+					onClick={() => {
+						this.setState({ chatMode: true });
+					}}
+				/>
+			</div>
+		);
+	}
+}
